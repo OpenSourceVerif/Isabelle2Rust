0aa7f09554580f6487c2f89755af3391affc334d
fisherfg19 <FisherFG@outlook.com>
2026-04-29 16:59:35 +0800

    feat(rust_setup): basic integers mapping via num-bigint

--- Rust_Setup.thy
+++ Rust_Setup.thy
@@ -5,10 +5,6 @@ begin
 ML_file \<open>code_debug_info.ML\<close>
 ML_file \<open>code_rust.ML\<close>
 
-code_identifier
-  code_module Code_Target_Nat \<rightharpoonup> (Rust) Arith
-| code_module Code_Target_Int \<rightharpoonup> (Rust) Arith
-| code_module Code_Numeral \<rightharpoonup> (Rust) Arith
 
 code_printing
   constant Code.abort \<rightharpoonup> (Rust) "panic!( _ )"
@@ -42,4 +38,50 @@ setup \<open>
   fold Literal.add_code ["Rust"]
 \<close>
 
+(* Integers via num-bigint *)
+
+code_printing
+  type_constructor "integer" \<rightharpoonup> 
+    (Rust) "BigInt"
+
+code_printing
+  constant "0 :: integer" \<rightharpoonup> 
+    (Rust) "BigInt::ZERO"
+
+setup \<open>
+Numeral.add_code \<^const_name>\<open>Code_Numeral.Pos\<close> I Code_Printer.literal_numeral "Rust"
+#> Numeral.add_code \<^const_name>\<open>Code_Numeral.Neg\<close> (~) Code_Printer.literal_numeral "Rust"
+\<close>
+
+code_printing
+  constant "plus :: integer \<Rightarrow> _ \<Rightarrow> _" \<rightharpoonup>
+    (Rust) infixl 6 "+"
+| constant "uminus :: integer \<Rightarrow> _" \<rightharpoonup>
+    (Rust) "!(- _)"
+| constant "minus :: integer \<Rightarrow> _" \<rightharpoonup>
+    (Rust) infixl 6 "-"
+| constant Code_Numeral.dup \<rightharpoonup>
+    (Rust) "!(_ << 1usize)"
+| constant Code_Numeral.sub \<rightharpoonup>
+    (Rust) "panic'!(\"sub\")"
+| constant "times :: integer \<Rightarrow> _ \<Rightarrow> _" \<rightharpoonup>
+    (Rust) infixl 7 "*"
+| constant Code_Numeral.divmod_abs \<rightharpoonup>
+    (Rust) "!(match (_, _) { (k, l) => { let k = if k < BigInt::ZERO { -k } else { k }; let l = if l < BigInt::ZERO { -l } else { l }; if l == BigInt::ZERO { (BigInt::ZERO, k) } else { let q = &k / &l; let r = k % l; (q, r) } } })"
+| constant "HOL.equal :: integer \<Rightarrow> _ \<Rightarrow> bool" \<rightharpoonup>
+    (Rust) infix 4 "=="
+| constant "less_eq :: integer \<Rightarrow> _ \<Rightarrow> bool" \<rightharpoonup>
+    (Rust) infix 4 "<="
+| constant "less :: integer \<Rightarrow> _ \<Rightarrow> bool" \<rightharpoonup>
+    (Rust) infix 4 "<"
+| constant "abs :: integer \<Rightarrow> _" \<rightharpoonup>
+    (Rust) "!(match _ { k => if k < BigInt::ZERO { -k } else { k } })"
+| constant "Bit_Operations.and :: integer \<Rightarrow> integer \<Rightarrow> integer" \<rightharpoonup>
+    (Rust) infixl 7 "&"
+| constant "Bit_Operations.or :: integer \<Rightarrow> integer \<Rightarrow> integer" \<rightharpoonup>
+    (Rust) infixl 5 "|"
+| constant "Bit_Operations.xor :: integer \<Rightarrow> integer \<Rightarrow> integer" \<rightharpoonup>
+    (Rust) infixl 6 "^"
+| constant "Bit_Operations.not :: integer \<Rightarrow> integer" \<rightharpoonup>
+    (Rust) "'! _"
 end
