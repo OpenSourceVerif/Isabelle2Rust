f6cec252ca9471f4c7622c56935cf98db9f5f448
lujiayi <lucia20021008@gmail.com>
2026-07-14 03:49:07 +0800

    fix(codegen): preserve nested module paths

--- test/unit/functions/ModuleResolution_Test.thy
+++ test/unit/functions/ModuleResolution_Test.thy
@@ -13,12 +13,27 @@ definition beta :: nat where
 definition use_both :: nat where
   "use_both = alpha + beta"
 
+definition nested_alpha :: nat where
+  "nested_alpha = 0"
+
+definition nested_beta :: nat where
+  "nested_beta = 0"
+
+definition nested_sum :: nat where
+  "nested_sum = nested_alpha + nested_beta"
+
+definition use_nested :: nat where
+  "use_nested = nested_sum"
+
 code_identifier
   constant alpha \<rightharpoonup> (Rust) "ModA.dup"
 | constant beta  \<rightharpoonup> (Rust) "ModB.dup"
+| constant nested_alpha \<rightharpoonup> (Rust) "Outer.ModA.dup"
+| constant nested_beta  \<rightharpoonup> (Rust) "Outer.ModB.dup"
+| constant nested_sum   \<rightharpoonup> (Rust) "Outer.ModA.sum"
 
 export_code
-  use_both
+  use_both use_nested
   in Rust
 
 end
