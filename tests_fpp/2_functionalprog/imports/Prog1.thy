2a4ca4c6f96a645185c41c5af0c35c861a9a234b
lujiayi <lucia20021008@gmail.com>
2026-08-21 21:35:27 +0800

    test: remove superseded FPP sources

--- tests_fpp/2_functionalprog/imports/Prog1.thy
+++
@@ -1,6 +0,0 @@
-theory Prog1
-imports Main
-begin
-definition "add x y \<equiv> (x :: int) + y"
-definition "sub x y \<equiv> (x::int) - y"
-end
--- tests_fpp/2_functionalprog/imports/Prog3.thy
+++
@@ -1,7 +0,0 @@
-theory Prog3
-imports Main Prog1 Prog2
-begin
-value "sub 1 2"
-value "Prog1.sub 1 2"
-value "Prog2.sub 1 2"
-end
